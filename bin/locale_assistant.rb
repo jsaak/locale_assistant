d98202e4b76b90f8f6d3fc8fa55f532d7d017cee
jsaak <jsaak@napalm.hu>
2013-03-04 15:15:57 +0100

    initial version

---
+++ bin/locale_assistant.rb
@@ -0,0 +1,182 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+begin
+   require Dir.pwd+'/config/locale_assistant.conf.rb'
+rescue LoadError
+   puts 'Needs a config file located at CURRENT_DIR/config/locale_assistant.conf.rb'
+   puts 'something like this:'
+   puts %Q|
+module LocaleAssistant
+   Files = ['config/locales/#lang#.yml',
+            'config/locales/#lang#_models.yml',
+            'vendor/engines/contract_management/config/locales/#lang#.yml',
+            'vendor/engines/contract_management/config/locales/#lang#_models.yml' ]
+   Languages = ['hu','en','he']
+   IgnoreList = []
+end
+|
+end
+
+def load_file(fn)
+   arr = []
+   hash = {}
+
+   keystore = []
+   indentstore = []
+   spaces = -1
+
+   f = File.open(fn)
+   f.each_line do |line|
+      curr_spaces = (line.size - line.lstrip.size)
+      key,val = line.strip.split(/:/,2)
+      val = val.strip
+
+      if curr_spaces > spaces
+         indentstore.push(curr_spaces-spaces)
+         spaces = curr_spaces
+         keystore.push(key)
+      elsif curr_spaces == spaces
+         keystore.pop
+         keystore.push(key)
+      elsif curr_spaces < spaces
+         while curr_spaces <= spaces
+            spaces -= indentstore.pop
+            keystore.pop
+         end
+         keystore.push(key)
+      end
+
+      unless val == ''
+         x = keystore.join('.')
+         hash[x] = val
+         arr.push([x,val])
+      end
+   end
+   f.close
+
+   arr.sort!
+   return [arr,hash]
+end
+
+def write_out(fn,array,hash,country_code)
+  f = File.new(fn,'w+')
+  prev_x = []
+  indent = 0
+
+  array.each do |key,val|
+    next if LocaleAssistant::IgnoreList.include?(key)
+    x = key.split('.')
+    x[0] = country_code
+
+    counter = 0
+    prev_x.each do |fragment|
+      counter +=1
+      break if x[counter-1] != fragment
+    end
+
+    indent = 0
+    counter -= 1 if counter > 1
+    x[counter..-1].each do |fragment|
+      f << ('  '*(counter+indent)) + fragment + ':'
+      if indent+1 == x[counter..-1].size
+        value2 = hash[country_code+key[2..-1]]
+        if value2.nil?
+          f << ' "TODO ('+val.to_s+')"'
+        else
+          f << ' "' + value2.to_s + '"'
+        end
+      end
+      f << "\n"
+      indent += 1
+    end
+
+    prev_x = x
+  end
+end
+
+def process_file(path,file,source,inspecting_mode,destructive,use_tempfile)
+  arrays = {}
+  hashes = {}
+  LocaleAssistant::Languages.each do |lang|
+    arrays[lang],hashes[lang] = load_file(path + file.gsub('#lang#',lang))
+  end
+
+  arrays[source].each do |key,val|
+    general_key = key[2..-1]
+    (LocaleAssistant::Languages-[source]).each do |lang|
+      localized_key = lang+general_key
+      unless hashes[lang].include?(localized_key)
+        if LocaleAssistant::IgnoreList.include?(localized_key)
+          #puts fn_beg+lang+fn_end + ' ignoring:' + localized_key
+        else
+          arrays[lang] << [localized_key,'??? '+val.to_s]
+          puts file.gsub('#lang#',lang) + ' MISSING: ' + localized_key
+        end
+      end
+    end
+  end
+
+  #ignore lists
+  LocaleAssistant::IgnoreList.each do |key|
+    lang = key[0..1]
+    base_key = key[3..-1]
+    arrays[lang] << [key,hashes[lang][base_key]]
+  end
+
+  if inspecting_mode == false
+    LocaleAssistant::Languages.each do |lang|
+      (arrays[lang]).sort! {|a,b| a[0] <=> b[0]}
+      language_to_use = lang
+      language_to_use = source if destructive
+
+      file_suffix = ''
+      file_suffix = '.temp' if use_tempfile
+
+      write_out(path + file.gsub('#lang#',lang)+file_suffix,arrays[language_to_use],hashes[lang],lang)
+    end
+  end
+end
+
+def parse_command_line
+  options = {}
+
+  optparse = OptionParser.new do |opts|
+    opts.banner = "Usage: #{__FILE__} [options] source_language"
+
+    options[:destructive] = false
+    opts.on( '-D', '--destructive', 'If you want to clear keys that are not in the source language' ) do
+      options[:destructive] = true
+    end
+
+    options[:tempfile] = false
+    opts.on( '-t', '--tempfile', 'Use .temp files for inspecting before overwriting' ) do
+      options[:tempfile] = true
+    end
+
+    options[:inspecting_mode] = false
+    opts.on( '-i', '--inspecting-mode', 'Do not make changes, just list the missing keys' ) do
+      options[:inspecting_mode] = true
+    end
+
+    opts.on( '-h', '--help', 'Display this screen' ) do
+      puts opts
+      exit
+    end
+  end
+
+  optparse.parse!
+  return options
+end
+
+options = parse_command_line
+
+if LocaleAssistant::Languages.include?(ARGV[0])
+  puts "using " + ARGV[0] + " as source language"
+  LocaleAssistant::Files.each do |file|
+    process_file(path,file,ARGV[0],options[:inspecting_mode],options[:destructive],options[:tempfile])
+  end
+else
+  puts "Usage: #{__FILE__} [options] source_language"
+end
---
+++ locale_assistant.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+   s.name        = 'locale_assistant'
+   s.version     = '0.9.0'
+   s.date        = '2013-03-04'
+   s.summary     = "Helps you manage those big locale yaml files"
+   s.description = %Q|locale_assistant is lightweight commandline tool
+which helps you manage those big yaml files when using rails and multiple languages
+if you want to keep things simple and do not need online services like localeapp|
+   s.authors     = ["jsaak"]
+   s.email       = 'jsaak@email.to'
+   s.executables << 'locale_assistant.rb'
+   s.homepage    = 'https://github.com/jsaak/locale_assistant'
+end
